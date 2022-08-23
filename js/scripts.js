9db884087be3cf0b3e6789db746511a3f75e5a8f
MatiLoures <matiasloures@gmail.com>
2022-08-22 21:59:52 -0300

    Optimizar1

--- js/scripts.js
+++ js/scripts.js
@@ -2,51 +2,48 @@ const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
 document.getElementById ('carritoTotal').innerHTML = carrito.length;
 
 const producto = [
-    {
-        id:1, 
-        nombre: 'Triceratops',
-        categoria: 'Productos',
-        img: '../img/Tricera.png',
-        category: 'Animales', 
-        precio: 1600,
-    },
-    {
-        id:2, 
-        nombre: 'Dino', 
-        categoria: 'Productos',
-        category: 'Animales', 
-        precio: 1700
-    },
-    {
-        id:3, 
-        nombre: 'Hipopotas',
-        categoria: 'Productos',
-        category: 'Animales',
-        precio: 2000
-    },
-    {
-        id:4, 
-        nombre: 'Principito',
-        categoria: 'Productos',
-        category: 'Personajes', 
-        precio: 2500
-    },
-    {
-        id:5, 
-        nombre: 'Batman',
-        categoria: 'Productos',
-        category: 'Personajes',  
-        precio: 2500
-    },
-    {
-        id:6, 
-        nombre: 'Canasta de Frutas',
-        categoria: 'Productos',
-        category: 'Objetos',  
-        precio: 3000
-    },
+    {id:1, nombre: 'Ranita', categoria: 'Productos', category: 'Animales', precio: 1600, img: './img/Rana.png'},
+    {id:2, nombre: 'Osito Nordico', categoria: 'Productos', category: 'Animales', precio: 1700, img: './img/Oso.png'},
+    {id:7, nombre: 'Ciervo Nordico', categoria: 'Productos', category: 'Animales', precio: 2000, img: './img/Ciervo.png'},
+    {id:3, nombre: 'Hipopotas', categoria: 'Productos', category: 'Animales', precio: 2000, img: './img/Hipo.png'},
+    {id:4, nombre: 'Frida', categoria: 'Productos', category: 'Personajes', precio: 2500, img: './img/frida.png'},
+    {id:5, nombre: 'Batman', categoria: 'Productos', category: 'Personajes', precio: 2500, img: './img/Batman.png'},
+    {id:6, nombre: 'Canasta de Frutas', categoria: 'Productos', category: 'Objetos', precio: 3000, img: './img/BolsaFrutas.png'},   
 ];
 
+//Todos los productos
+for (const nodoHTML of document.getElementsByClassName('TodosLoProductos')) {
+    nodoHTML.onclick = (event) => {
+        const categoria = event.target.getAttribute('data-categoria')
+        filtrarTodosLosProductos(categoria)
+    } ;
+} 
+
+function filtrarTodosLosProductos (categoria) {
+    document.getElementById("seccion-card").innerHTML = "";
+    const todosLosProductos = producto.filter((producto) => producto.categoria === categoria);
+
+    todosLosProductos.forEach((producto) => { 
+        const idButton = `add-cart${producto.id}`
+        document.getElementById("seccion-card").innerHTML += `<div class="col mb-5">
+                <div class="card h-100">
+                    <img class="card-img-top" src=${producto.img}>
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <h5 class="fw-bolder">${producto.nombre}</h5>
+                            <p>$${producto.precio}</p>
+                        </div>
+                    </div>
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="text-center" id="${idButton}"><a class="btn btn-outline-dark mt-auto" href="#">Agregar</a></div>
+                    </div>
+                </div>
+             </div>`;
+        })
+    
+}
+
+//Categorias
 for (const nodoHTML of document.getElementsByClassName('filtrarCategorias')) {
     nodoHTML.onclick = (event) => {
         const category = event.target.getAttribute('data-category')
@@ -62,7 +59,7 @@ function filtrarProductosPorCategoria (category) {
         const idButton = `add-cart${producto.id}`
         document.getElementById("seccion-card").innerHTML += `<div class="col mb-5">
                 <div class="card h-100">
-                    <img class="card-img-top" src=${producto.img}/>
+                    <img class="card-img-top" src=${producto.img}>
                     <div class="card-body p-4">
                         <div class="text-center">
                             <h5 class="fw-bolder">${producto.nombre}</h5>
@@ -82,7 +79,7 @@ producto.forEach((producto) => {
 const idButton = `add-cart${producto.id}`
 document.getElementById("seccion-card").innerHTML += `<div class="col mb-5">
         <div class="card h-100">
-            <img class="card-img-top" src="../img/${producto.img}.png"/>
+            <img class="card-img-top" src=${producto.img}>
             <div class="card-body p-4">
                 <div class="text-center">
                     <h5 class="fw-bolder">${producto.nombre}</h5>
@@ -95,6 +92,7 @@ document.getElementById("seccion-card").innerHTML += `<div class="col mb-5">
         </div>
      </div>`;
 })
+
 producto.forEach((producto) => { 
     const idButton = `add-cart${producto.id}`
     document.getElementById(idButton).onclick = () => {
@@ -103,12 +101,11 @@ producto.forEach((producto) => {
         localStorage.setItem('carrito', JSON.stringify(carrito));
         Swal.fire({
             icon: 'success',
-            title: 'Your work has been saved',
+            title: 'Se agrego al carrito',
             showConfirmButton: false,
             timer: 1500
           });
         }
         console.log(carrito);
-
 })
 
